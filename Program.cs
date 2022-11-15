ef9e0842041df8d2317828d7d1fd46031d05ea3b
glyceryl <494192175@qq.com>
2022-11-16 00:57:13 +0800

    尝试恢复主窗体

--- Program.cs
+++ Program.cs
@@ -1,22 +1,14 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: MinecraftSlashBladeGenerator.Program
-// Assembly: MinecraftSlashBladeGenerator, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: FB172600-17B5-47DA-A94D-A55A7C8CD28B
-// Assembly location: E:\Microsoft Visual Studio\CSharpCode\MinecraftSlashBladeGenerator\obj\Debug\MinecraftSlashBladeGenerator.exe
-
-using System;
+﻿using System;
 using System.Windows.Forms;
 
-namespace MinecraftSlashBladeGenerator
-{
-  internal static class Program
-  {
+namespace MinecraftSlashBladeGenerator;
+
+  internal static class Program {
     [STAThread]
-    private static void Main()
-    {
+    private static void Main() {
       Application.EnableVisualStyles();
       Application.SetCompatibleTextRenderingDefault(false);
-      Application.Run((Form) new Form1());
+      Application.Run(new Form1());
     }
+    
   }
-}
